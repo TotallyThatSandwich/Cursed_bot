944b9a07463629faae8758f3bbf1db0a7aa36130
ThatSandwich <hamishheath2008@gmail.com>
2024-02-23 17:58:19 +1100

    status command added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import os
 from dotenv import load_dotenv
+from typing import Literal
 
 load_dotenv()
 token = os.getenv("TOKEN") 
@@ -15,6 +16,20 @@ bot = commands.Bot(command_prefix='?', intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="Hazbin Hotel"))
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="set_status")
+@app_commands.describe(statustext = "what should my status say?")
+async def set_status(interaction: discord.Interaction, statustype: Literal['watching', 'playing'] , statustext: str):
+    if statustype == 'watching':
+        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=statustext))
+    else:
+        await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=statustext))
 
 bot.run(token)
