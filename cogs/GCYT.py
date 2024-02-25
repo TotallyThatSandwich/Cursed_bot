2b62f11554ea50f4aa3d39a9e0596fa278735ab0
ThatSandwich <hamishheath2008@gmail.com>
2024-02-25 14:45:17 +1100

    added GCYT task

---
+++ cogs/GCYT.py
@@ -0,0 +1,107 @@
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+import os
+import sys
+import aiofiles
+import requests
+
+sys.path.append(os.path.abspath("../"))
+
+import settings as settings
+
+logger = settings.logging.getLogger("bot")
+
+class GCYT(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.retrieve_videos.start()
+
+    @tasks.loop(minutes=1)
+    async def retrieve_videos(self):
+        video = await self.retrieveNewestGCGSVideo()
+        if video != None:
+            try:
+                channel = self.bot.get_channel(int(settings.GCYTC))
+                await channel.send(video)
+            except Exception as e:
+                logger.info(e)
+        else:
+            try:
+                channel = self.bot.get_channel(int(settings.GCYTC))
+                await channel.send("no new videos")
+            except Exception as e:
+                logger.info(e)
+
+    async def retrieveNewestGCGSVideo(self):
+        URL = "https://www.googleapis.com/youtube/v3/channels"
+        channelID = "UCG6DkyglRHZxtTP65FDgj1w"
+        youtubeAPIkey = os.getenv("GOOGLEAPI")
+        fetchParameters = {
+            "part":"contentDetails",
+            "id": channelID,
+            "key": youtubeAPIkey}
+
+        response = requests.get(url= URL, params=fetchParameters)
+        response = response.json()
+        
+        playlistID = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
+        URL = "https://www.googleapis.com/youtube/v3/playlistItems"
+        fetchParameters = {
+            "part": "contentDetails",
+            "playlistId": playlistID,
+            "maxResults": 1,
+            "key": youtubeAPIkey
+        }
+
+        response = requests.get(url=URL, params=fetchParameters)
+        response = response.json()
+
+        videoID = response["items"][0]["contentDetails"]["videoId"]
+
+        saved_video_id = await self.read_saved_video_id()
+        if videoID != saved_video_id:
+            await self.save_video_id(videoID)
+            return "https://www.youtube.com/watch?v=" + videoID
+        else:
+            return None
+    
+    async def save_video_id(self, video_id):
+        async with aiofiles.open("../latest_video_id.txt", "w") as f:
+            await f.write(video_id)
+
+    async def read_saved_video_id(self):
+        try:
+            async with aiofiles.open("../latest_video_id.txt", "r") as f:
+                return (await f.read()).strip()
+        except FileNotFoundError:
+            return None
+        
+    #@app_commands.command(name="latest_video", description="Get latest GCGS youtube video")
+    async def getLatestVideo(self, interaction: discord.Interaction):
+        video = await self.retrieveNewestGCGSVideo()
+        
+        if video != None:
+            try:
+                channel = self.bot.get_channel(int(settings.GCYTC))
+                await channel.send(video)
+
+            except Exception as e:
+                logger.info(e)
+                await interaction.response.send_message(video)
+
+        else:
+            videoID = await self.read_saved_video_id()
+            video = "https://www.youtube.com/watch?v=" + videoID
+
+            try:
+                channel = self.bot.get_channel(int(settings.GCYTC))
+                await channel.send(video)
+
+            except Exception as e:
+                logger.info(e)
+                await interaction.response.send_message(video)
+
+async def setup(bot):
+    await bot.add_cog(GCYT(bot))
--- main.py
+++ main.py
@@ -32,78 +32,76 @@ async def on_ready():
     logger.info(f'Logged in as {bot.user} (ID: {bot.user.id})')
 
 ## Retrieve new GCGS video
-async def retrieveNewestGCGSVideo():
-    URL = "https://www.googleapis.com/youtube/v3/channels"
-    channelID = "UCG6DkyglRHZxtTP65FDgj1w"
-    youtubeAPIkey = os.getenv("GOOGLEAPI")
-    fetchParameters = {
-         "part":"contentDetails",
-         "id": channelID,
-         "key": youtubeAPIkey}
-
-    response = requests.get(url= URL, params=fetchParameters)
-    response = response.json()
-    
-    playlistID = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
-
-    URL = "https://www.googleapis.com/youtube/v3/playlistItems"
-    fetchParameters = {
-        "part": "contentDetails",
-        "playlistId": playlistID,
-        "maxResults": 1,
-        "key": youtubeAPIkey
-    }
-
-    response = requests.get(url=URL, params=fetchParameters)
-    response = response.json()
-
-    videoID = response["items"][0]["contentDetails"]["videoId"]
-
-    saved_video_id = await read_saved_video_id()  # Function to read saved video ID
-    if videoID != saved_video_id:
-        await save_video_id(videoID)  # Function to save the latest video ID
-        return "https://www.youtube.com/watch?v=" + videoID
-    else:
-        return None  # No new video
-    
-
-async def save_video_id(video_id):
-    async with aiofiles.open("latest_video_id.txt", "w") as f:
-        await f.write(video_id)
-
-async def read_saved_video_id():
-    try:
-        async with aiofiles.open("latest_video_id.txt", "r") as f:
-            return (await f.read()).strip()
-    except FileNotFoundError:
-        return None
-
-@bot.tree.command(name="latest_video", description="Get latest GCGS youtube video")
-async def getLatestVideo(interaction: discord.Interaction):
-    video = await retrieveNewestGCGSVideo()\
-    
-    if video != None:
-        try:
-            channel = bot.get_channel(int(settings.GCYTC))
-            await channel.send(video)
-
-        except Exception as e:
-            logger.info(e)
-            await interaction.response.send_message(video)
-
-    else:
-        videoID = await read_saved_video_id()
-        video = "https://www.youtube.com/watch?v=" + videoID
-
-        try:
-            channel = bot.get_channel(int(settings.GCYTC))
-            await channel.send(video)
-            
-        except Exception as e:
-            logger.info(e)
-            await interaction.response.send_message(video)
+#async def retrieveNewestGCGSVideo():
+#    URL = "https://www.googleapis.com/youtube/v3/channels"
+#    channelID = "UCG6DkyglRHZxtTP65FDgj1w"
+#    youtubeAPIkey = os.getenv("GOOGLEAPI")
+#    fetchParameters = {
+#         "part":"contentDetails",
+#         "id": channelID,
+#         "key": youtubeAPIkey}
+#
+#    response = requests.get(url= URL, params=fetchParameters)
+#    response = response.json()
+#    
+#    playlistID = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+#
+#    URL = "https://www.googleapis.com/youtube/v3/playlistItems"
+#    fetchParameters = {
+#        "part": "contentDetails",
+#        "playlistId": playlistID,
+#        "maxResults": 1,
+#        "key": youtubeAPIkey
+#    }
+#
+#    response = requests.get(url=URL, params=fetchParameters)
+#    response = response.json()
+#
+#    videoID = response["items"][0]["contentDetails"]["videoId"]
+#
+#    saved_video_id = await read_saved_video_id()  # Function to read saved video ID
+#    if videoID != saved_video_id:
+#        await save_video_id(videoID)  # Function to save the latest video ID
+#        return "https://www.youtube.com/watch?v=" + videoID
+#    else:
+#        return None  # No new video
+#    
+#
+#async def save_video_id(video_id):
+#    async with aiofiles.open("latest_video_id.txt", "w") as f:
+#        await f.write(video_id)
+#
+#async def read_saved_video_id():
+#    try:
+#        async with aiofiles.open("latest_video_id.txt", "r") as f:
+#            return (await f.read()).strip()
+#    except FileNotFoundError:
+#        return None
+#
+#@bot.tree.command(name="latest_video", description="Get latest GCGS youtube video")
+#async def getLatestVideo(interaction: discord.Interaction):
+#    video = await retrieveNewestGCGSVideo()
+#    
+#    if video != None:
+#        try:
+#            channel = bot.get_channel(int(settings.GCYTC))
+#            await channel.send(video)
+#
+#        except Exception as e:
+#            logger.info(e)
+#            await interaction.response.send_message(video)
+#
+#    else:
+#        videoID = await read_saved_video_id()
+#        video = "https://www.youtube.com/watch?v=" + videoID
+#
+#        try:
+#            channel = bot.get_channel(int(settings.GCYTC))
+#            await channel.send(video)
+#
+#        except Exception as e:
+#            logger.info(e)
+#            await interaction.response.send_message(video)
         
-
-
 
 bot.run(settings.TOKEN, root_logger=True)
