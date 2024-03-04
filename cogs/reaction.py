98a4badceb7d90d29bb74f28769c8d8181a591ee
ThatSandwich <hamishheath2008@gmail.com>
2024-03-04 11:35:26 +1100

    added chance for nerd react and multiple nerd reactions

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -10,20 +10,25 @@ sys.path.append(os.path.abspath("../"))
 import settings 
 logger = settings.logging.getLogger("bot")
 
+nerds = ["https://tenor.com/view/nerd-emoji-nerd-emoji-avalon-play-avalon-gif-24241051", "https://tenor.com/view/pon-gif-15097379091171620462"]
+
 class react(commands.Cog):
-    
+
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        
+
         messageContent = str(message.content)
 
         if "reply" in str(message.type):
             if not ("*" in messageContent and len(messageContent) > 1 and " " not in messageContent):
                 return
-            await message.reply("https://tenor.com/view/pon-gif-15097379091171620462")
+            # 75% chance to react
+            if random.randint(0, 3) == 1:
+                return
+            await message.reply(random.choice(nerds))
         else:
             logger.info(message, message.type)
 
