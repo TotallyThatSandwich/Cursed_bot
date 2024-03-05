03228731849cbcc57fc43491f847c343e6bfa69c
GCGS_l4k5 <84022277+GCGS-l4k5@users.noreply.github.com>
2024-03-05 19:04:43 +1100

    fixed trainining

--- cogs/genericAI.py
+++ cogs/genericAI.py
@@ -12,28 +12,30 @@ from chatterbot.trainers import ListTrainer
 genericAI = ChatBot("genericAI")
 trainer = ListTrainer(genericAI)
 
-class genericAI(commands.cog):
+class genericAI(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if ["1210389365185056818", "1210512184103403530"] in message.content:
+        if "1210389365185056818" in message.content or "1210512184103403530" in message.content:
             response = genericAI.get_response(message.content)
             await message.channel.send(response)
         elif "reply" in str(message.type) and ["1210389365185056818", "1210512184103403530"] in message.mentions:
             response = genericAI.get_response(message.content)
             await message.channel.send(response)
     
-    @app_commands.command()
-    async def trainMessageData(self, ctx, limit):
-        if limit > 50:
-            return await ctx.send("Limit must be less than 50")
-        messageTrain = ctx.channel.fetch_message(limit=limit)
+    @app_commands.command(name="train_bot", description="Train the bot with the last x messages in the channel")
+    @app_commands.describe(limit = "How many messages should the bot be trained with?")
+    async def trainMessageData(self, interaction: discord.Interaction, limit: int = None):
+        if limit > 50 or limit == None:
+            return await interaction.response.send_message("Limit must be less than 50 or not empty")
+        messageTrain = interaction.channel.history(limit=limit)
+
         formattedMessageTrain = []
         for i in messageTrain:
             formattedMessageTrain.append(str(i).lower())
-
+        
         trainer.train(formattedMessageTrain)
 
 async def setup(bot):
