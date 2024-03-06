48a8bef461c29b31ea6293702e5dce011368baf8
ThatSandwich <hamishheath2008@gmail.com>
2024-03-07 10:14:30 +1100

    fixed shittie ah code [def not wrong branch]

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -31,19 +31,26 @@ class react(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.Cog.listener()
     async def on_message(self, message):
 
-        with open("../optoutlist.txt", "r") as optoutlist:
-            optoutlistLines = optoutlist.readlines()
-            for i in optoutlistLines:
-                if i == str(message.author.id):
-                    return
-                
+        with open("optoutlist.txt", "r") as optoutlist:
+                optoutlistLines = optoutlist.readlines()
+                for i in optoutlistLines:
+                    if str(message.author.id) in i:
+                        return 
+                    
+        if "1024094820706287697" in str(message.channel.id):
+            return 
+        
+        if str(message.author.id) in ["1210389365185056818", "1210512184103403530"]:
+            return 
+        
         messageContent = str(message.content).lower()
 
         # Use this if condition for any messages that are replying to others. These listeners don't listen for the bot itself.
-        if "reply" in str(message.type) and not(str(message.author.id) in ["1210389365185056818", "1210512184103403530"]):
+        if "reply" in str(message.type):
             if "*" in messageContent and len(messageContent) > 1 and " " not in messageContent:
                 # 75% chance to react
                 if random.randint(1, 100) <= 75:
@@ -59,7 +66,6 @@ class react(commands.Cog):
         # Normal message listners go below
         
         if "crazy" in messageContent:
-            if str(message.author.id) not in ["1210389365185056818", "1210512184103403530"]:
                 await message.reply("Crazy? I was crazy once. They locked me in a room. A rubber room! A rubber room with rats, and rats make me crazy.")
             # elif str(message.author.id) in ["1210389365185056818", "1210512184103403530"] and "reply" in str(message.type):
             #     if random.randint(1,2) == 1:
@@ -90,16 +96,16 @@ class react(commands.Cog):
             elif wordType[0][1]=="PRP":
                 await message.reply(f"If they have a million fans, then I am one of them. If they have ten fans, then I am one of them. If they have only one fan then that is me. If they have no fans, then that means I am no longer on earth. If the world is against them, then I am against the world.")
 
-        if "bill" in messageContent and not (str(message.author.id) in ["1210389365185056818", "1210512184103403530"]):
+        if "bill" in messageContent:
             await message.reply("What the ♥♥♥♥ is wrong with this old bastard? Homeboy just straight up dies mid-level, the ♥♥♥♥ is this ♥♥♥♥? Dumb ♥♥♥♥ obviously missed the directions for the fountain of youth. Senile old ♥♥♥♥ can't even hold his own breath for 2 seconds without coughing up a lung, eat ♥♥♥♥ Bill. I like the science dude better.")
         
-        if "sus" in messageContent and not (str(message.author.id) in ["1210389365185056818", "1210512184103403530"]):
+        if "sus" in messageContent:
             replies = ["STOP POSTING ABOUT AMONG US! I'M TIRED OF SEEING IT! My friends on TikTok send me memes, on Discord its fucking memes. I was in a server, right, and ALL the channels are just Among Us stuff. I showed my Champion underwear to my girlfriend, and the logo I flipped it and I said \"Hey babe, when the underwear sus! HAHA! Ding Ding Ding Ding Ding Ding Ding DiDiDing!\" I fucking looked at a trash can and I said \"Thats a bit sussy!\" I looked at my penis, I thought of the astronauts helmet and I go \"PENIS? MORE LIKE PEN-SUS!\" AAAAAAAAAAAAAA", 
                        "I can't fucking take it any more. Among Us has singlehandedly ruined my life. The other day my teacher was teaching us Greek Mythology and he mentioned a pegasus and I immediately thought 'Pegasus? more like Mega Sus!!!!' and I've never wanted to kms more. I can't look at a vent without breaking down and fucking crying. I can't eat pasta without thinking 'IMPASTA??? THATS PRETTY SUS!!!!' Skit 4 by Kanye West. The lyrics ruined me. A Mongoose, or the 25th island of greece. The scientific name for pig. I can't fucking take it anymore. Please fucking end my suffering.",
                        "Red sus. Red suuuus. I said red, sus, hahahahaha. Why arent you laughing? I just made a reference to the popular video game \"Among Us\"! How can you not laugh at it? Emergeny meeting! Guys, this here guy doesnt laugh at my funny Among Us memes! Lets beat him to death! Dead body reported! Skip! Skip! Vote blue! Blue was not an impostor. Among us in a nutshell hahahaha. What?! Youre still not laughing your ass off? I made SEVERAL funny references to Among Us and YOU STILL ARENT LAUGHING??!!! Bruh. Ya hear that? Wooooooosh. Whats woooosh? Oh, nothing. Just the sound of a joke flying over your head. Whats that? You think im annoying? Kinda sus, bro. Hahahaha! Anyway, yea, gotta go do tasks. Hahahaha!"]
             await message.reply(replies[random.randint(0,len(replies)-1)])
         
-        if "touch grass" in messageContent and not(str(message.author.id) in ["1210389365185056818", "1210512184103403530"]):
+        if "touch grass" in messageContent:
             await message.reply("\"touch grass\" is not an insult towards gamers, rather it is advice for them. When participating in intense periods of gaming, the human hand has a tendency to get sweaty. The sweat causes the hand to become slick, and it b becomes more difficult to retain a grip on the gamers gaming mouse, thus making it more difficult to perform well in intense gaming moments. By touching grass with the gamers hand, the grass will impart a layer of particulate onto the gamers hand, the particulate can be made of a variety of dusts, dirts and other natural matter. This particulate will then act in a similar form to climbers chalk, absorbing the sweat and drying out the gamers hand. With dry hands, the gamer can now perform to their maximum when gaming. This is why when an enemy or teammate tells you to touch grass, they are simply trying to assist you in performing better.")
         
         if "is so good" in messageContent and str(message.channel.name).lower() in competitiveShooters:
@@ -117,24 +123,26 @@ class react(commands.Cog):
 
             await message.reply(f"This {playerName} player is fantastic. Just needs to work on communication, aim, map awareness, crosshair placement, economy management, pistol aim, awp flicks, grenade spots, smoke spots, pop flashes, positioning, bomb plant positions, retake ability, bunny hopping, spray control and getting a kill.")
         
-        if random.randint(1, 100) == 1 and not(str(message.author.id) in ["1210389365185056818", "1210512184103403530"]):
+        if random.randint(1, 100) == 1:
             await message.reply(randomCopypastas[random.randint(0, len(randomCopypastas)-1)])
 
     @app_commands.command(name="opt-out", description="opt-out of the bot's responses")
     async def optOut(self, interaction: discord.Interaction):
-        with open("../optoutlist.txt", "w") as optoutlist:
+        with open("optoutlist.txt", "w") as optoutlist:
             optoutlist.write(f"{interaction.user.id}\n")
         await interaction.response.send_message("You have opted-out of bot's responses.",delete_after=10, ephemeral=True)
 
     @app_commands.command(name="opt-in", description="opt-in to the bot's responses")
     async def optIn(self, interaction: discord.Interaction):
-        with open("../optoutlist.txt", "r") as optoutlist:
-            optoutlistLines = optoutlist.readlines()
+        with open("optoutlist.txt", "r") as optoutlistRead:
+            optoutlistLines = optoutlistRead.readlines()
             for i in optoutlistLines:
-                if i == interaction.user.id:
+                if str(interaction.user.id) in i:
                     optoutlistLines.remove(i)
-        with open("../optoutlist.txt", "w") as optoutlist:
-            optoutlistLines.write(optoutlist)
+
+        with open("optoutlist.txt", "w") as optoutlistWrite:
+            for i in optoutlistLines:
+                optoutlistWrite.write(f"{i}\n")
         await interaction.response.send_message("You have been removed from the opt-out list.", delete_after=10,  ephemeral=True)
 
 async def setup(bot):
