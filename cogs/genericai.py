5dea36f5633dfd28f74144d1b28075928a7c0b7a
GCGS_l4k5 <84022277+GCGS-l4k5@users.noreply.github.com>
2024-03-07 00:07:40 +1100

    removed crazy chance response

--- cogs/genericai.py
+++ cogs/genericai.py
@@ -16,6 +16,8 @@ logger = settings.logging.getLogger("bot")
 genericChatBot = ChatBot("GenericBot")
 trainer = ListTrainer(genericChatBot)
 
+usableChannels = ["1028185142491107328"]
+
 conversation = [
     "Hello",
     "Hi there!",
@@ -96,7 +98,7 @@ class genericAI(commands.Cog):
         if "reply" in str(message.type):
             repliedMessage = await message.channel.fetch_message(message.reference.message_id)
 
-            if str(repliedMessage.author.id) == "1210389365185056818" or str(repliedMessage.author.id) == "1210512184103403530":
+            if str(repliedMessage.author.id) == "1210389365185056818" or str(repliedMessage.author.id) == "1210512184103403530" and (message.author.id != "1210389365185056818" or message.author.id != "1210512184103403530"):
                 response = await formatResponse(botQuery)
                 return await message.channel.send(response)
             elif not("1210389365185056818" in str(repliedMessage.content) or "1210512184103403530" in str(repliedMessage.content) or repliedMessage.author.id == message.author.id):
